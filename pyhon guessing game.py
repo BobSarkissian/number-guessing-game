cc54a57a146a8fa7cf371a7abbaa62a7938c18b7
BobSarkissian <sarkissianb@gmail.com>
2025-03-24 12:00:47 +0200

    Add files via upload
    number guessing game

---
+++ pyhon guessing game.py
@@ -0,0 +1,32 @@
+import random
+low=1
+high=100
+answer=random.randint(low,high)
+guesses=0
+is_running=True
+print ("--------python number guessing game-------")
+print(f"select number between {low} and {high}")
+
+while is_running:
+
+    guess=input("enter your guess:")
+
+    if guess.isdigit():
+        guess=int(guess)
+        guesses+=1
+        
+        if   guess<low  or guess > high:
+            print(f"Out of range! Please select a number between {low} and {high}")
+        elif guess< answer:
+            print("Too low! try again")
+        elif guess> answer:
+            print("Too high! try again")
+        else:
+            print(f"correct anser is: {answer:10}")
+            print(f"Number of guesses: {guesses:10}")
+            is_running=False
+            
+    
+    else:
+        print("invalid guess")
+        print(f"please select number between {low} and {high}")
